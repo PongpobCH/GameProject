3f73f7650726772061a6837950c28ed8c33db65f
Pongpob Chuennanthachai <khlangs456@gmail.com>
2024-08-02 02:57:52 +0700

    ADD Dialogue Conversation + Next text and remove previous Text.

---
+++ Gameproject/Assets/TextSystem/DialogueManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        sentences = new Queue<string>();
+        
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("Starting Conversation with " + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+
+        }
+
+        DisplayNextSentence();
+
+    }  
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    } 
+
+
+    void EndDialogue ()
+    {
+        Debug.Log("END");
+    }
+    
+
+
+    
+}
