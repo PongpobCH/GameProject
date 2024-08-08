a063b7e06b9791440f1822e93b43ffe63de6f1f4
Pongpob Chuennanthachai <khlangs456@gmail.com>
2024-08-08 23:32:58 +0700

    Read file from text files

--- Gameproject/Assets/TextSystem/FileManager.cs
+++ Gameproject/Assets/TextSystem/FileManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class FileManager
@@ -41,7 +43,30 @@ public class FileManager
    
    public static List<string> ReadTextAsset(string filePath, bool includeBlankLines = true)
    {
-    return null;
-   }
+     TextAsset asset = Resources.Load<TextAsset>(filePath);
+        if(asset == null)
+        {
+            Debug.LogError($"Asset not found: '{filePath}'");
+            return null;
+        }
 
+        return ReadTextAsset(asset, includeBlankLines);
+    }
+    public static List<string> ReadTextAsset(TextAsset asset, bool includeBlankLines = true)
+    {
+        List<string> lines = new List<string> ();
+        using (StringReader sr = new StringReader(asset.text))
+        {
+            while (sr.Peek() > 1)
+            {
+                string line = sr.ReadLine();
+                if (includeBlankLines|| !string.IsNullOrWhiteSpace (line))
+                    lines.Add(line);
+
+                
+            }
+        }
+
+        return lines;
+    }
 }
