ce2f9fdf241713e71c0d256a597257cd75e8caca
Pongpob Chuennanthachai <khlangs456@gmail.com>
2024-08-10 22:45:34 +0700

    ADD Text Parsing

---
+++ Gameproject/Assets/Test Folder/TestParsing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParsing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string line = "Speaker \"Dialogue Goes In here!\""; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Gameproject/Assets/TextSystem/DialogueParser.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace DIALOGUE
+
+{
+    public class DialogueParser
+        {
+            public static DialogueLine Parse(string rawLine)
+            {
+                Debug.Log($"Parsing line - '{rawLine}'");
+
+
+                (string speaker, string dialogue, string commands) = RipContent(rawLine);
+
+                return new DialogueLine(speaker, dialogue, commands);
+            }
+
+             private static (string, string, string) RipContent(string rawLine)
+            {
+            string speaker = "",  dialogue = "" , commands = "";
+
+            return (speaker, dialogue, commands);
+            }
+
+        }  
+}
