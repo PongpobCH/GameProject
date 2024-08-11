9cf5c0c1071da1e83b112af78cdcd2c0fb7ab83a
Pongpob Chuennanthachai <khlangs456@gmail.com>
2024-08-11 20:10:42 +0700

    EDIT text parsing

--- Gameproject/Assets/Test Folder/TestParsing.cs
+++ Gameproject/Assets/Test Folder/TestParsing.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DIALOGUE;
 using UnityEngine;
 
 public class TestParsing : MonoBehaviour
@@ -7,8 +8,10 @@ public class TestParsing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string line = "Speaker \"Dialogue Goes In here!\""; 
-        
+        string line = "Speaker \"Dialogue Goes In here!\"Command(arguemnts here)"; 
+
+        DialogueParser.Parse(line);
+
     }
 
     // Update is called once per frame
